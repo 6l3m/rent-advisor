01240f61da4f893bbaa74ed9d7e52e4235bc13d8
Clément POULAIN <cpoulain@wideagency.com>
2019-09-17 16:46:34 +0200

    material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => (
-  <div className="container">
-    <h1>CRUD App with Hooks</h1>
-    <div className="flex-row">
-      <div className="flex-large">
-        <h2>Add user</h2>
-      </div>
-      <div className="flex-large">
-        <h2>View users</h2>
-      </div>
-    </div>
-  </div>
+  <Header />
 );
 
 export default App;
