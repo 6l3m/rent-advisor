f8bfd8f1a0af2d76481ebd8312f1336cf814c320
Clément POULAIN <cpoulain@wideagency.com>
2019-09-18 16:48:46 +0200

    theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import Header from './components/Header';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import Header from './components/Header/Header';
+import './App.scss';
+import DarkTheme from './components/DarkTheme/DarkTheme';
 
-const App = () => (
-  <Header />
-);
+const App = () => {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark', // Switching the dark mode on is a single property value change.
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <DarkTheme />
+    </ThemeProvider>
+  );
+};
 
 export default App;
